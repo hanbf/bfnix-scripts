e72a984919fa9469528929b4881d0ff4bcb2d25e
Han Bing Feng <hangoglebf@gmail.com>
2014-06-09 14:21:49 +0800

    add diffmerge py script

---
+++ diffwrap_diffmerge.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+# Configure your favorite diff program here.
+DIFF = "/usr/local/bin/diffmerge.sh"
+# Subversion provides the paths we need as the last two parameters.
+LEFT  = sys.argv[-2]
+RIGHT = sys.argv[-1]
+# Call the diff command (change the following line to make sense for
+# your diff program).
+cmd = [DIFF, LEFT, RIGHT]
+os.execv(cmd[0], cmd)
+# Return an errorcode of 0 if no differences were detected, 1 if some were.
+# Any other errorcode will be treated as fatal.
