d7d8fd8d80d29c52080be1efe645d2f0fc4a1fb7
Han Bing Feng <hangoglebf@gmail.com>
2014-11-02 09:55:26 +0800

    - refactor to separate mac os scripts - remove emacs rc

---
+++ macos_baidu/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ln -f -s `pwd`/bash_profile ~/.bash_profile
+ln -f -s `pwd`/bashrc ~/.bashrc
+ln -f -s `pwd`/vimrc ~/.vimrc
+ln -f -s `pwd`/gitconfig ~/.gitconfig
+
+ln -f -s `pwd`/ext-diff.sh ~/bin/ext-diff.sh
+ln -f -s `pwd`/ext-merge.sh ~/bin/ext-merge.sh
+ln -f -s `pwd`/svn_ext_diff.py ~/bin/svn_ext_diff.py
+ln -f -s `pwd`/svn_ext_diff_p4merge.py ~/bin/svn_ext_diff_p4merge.py
+ln -f -s `pwd`/svn_ext_merge.py ~/bin/svn_ext_merge.py
+
+
